a940035ebed9ee21e48389692f3f8e4a0fcbb262
jewbre <jewbre18@gmail.com>
2018-04-19 14:15:52 +0200

    initial setup

---
+++ common/interfaces/Service.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 19/04/2018
+ * Time: 00:54
+ */
+
+namespace common\interfaces;
+
+
+interface Service
+{
+    public function doSomething(): string;
+}
---
+++ index-2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Awesome Vilim.
+ * Date: 19/04/2018
+ * Time: 01:01
+ */
+
+
+$list = array_fill(0, 10, 0);
+
+
+foreach ($list as $key => $item) {
+    echo $item;
+}
